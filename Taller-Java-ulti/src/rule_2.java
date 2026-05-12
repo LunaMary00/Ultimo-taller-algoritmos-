528cfaacc04689b13e7e8bfe47dc7c5d660099a3
LuLu <lcuesto@ucompensar.edu.co>
2026-05-12 17:48:38 -0500

    inicio de reto dos

---
+++ Taller-Java-ulti/src/rule_2.java
@@ -0,0 +1,34 @@
+public class rule_2 {
+
+    // metodo calcularPromedio
+public static double calcularPromedio (int [] notas){ 
+    double sumatoria = 0;
+    for (int i = 0 ; i < notas.length; i++) {
+        sumatoria += notas[i];
+    }
+    double promedio = sumatoria / notas.length;
+    return promedio;
+}
+// metodo buscarMaximo
+public static int buscarMaximo (int [] notas){
+    int maximo = notas[0];
+    for (int i = 1; i < notas.length; i++) {
+        if (notas[i] > maximo) {
+            maximo = notas[i];
+        }
+    }
+    return maximo;
+}
+//metodo buscarMinimo
+public static int buscarMinimo (int [] notas){
+    int minimo = notas [0];
+    for (int i = 1; i < notas.length; i++) {
+        if(notas[i] < minimo) {
+            minimo = notas[i];
+        }
+    }
+    return minimo;
+}
+
+
+}
