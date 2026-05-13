51d3fa08327bba39a462919595b66c314088a795
LuLu <lcuesto@ucompensar.edu.co>
2026-05-12 21:27:58 -0500

    Update reto_2.java

--- Taller-Java-ulti/src/reto_2.java
+++ Taller-Java-ulti/src/reto_2.java
@@ -29,6 +29,39 @@ public static int buscarMinimo (int [] notas){
     }
     return minimo;
 }
+//metodo contarAprobados
+public static int contarAprobados (int [] notas){
+    int sumaAprobados = 0;
+    for (int i = 0; i < notas.length; i++) {
+        if (notas[i] >= 60) {
+            sumaAprobados++;
+        }
+    }
+    return sumaAprobados; 
+        }
 
+    // metodo mostrarReporte
+    public static void mostrarReporte (String [] nombresEstudiantes,int [] notas) {
+
+        System.out.println("nombre:" + "\t" + "nota:" + "\t" + "estado:");
+    for (int i = 0; i < nombresEstudiantes.length; i++)
+     {
+       
+        System.out.println( nombresEstudiantes[i] + "\t" +notas[i] + "\t");
+       /* System.out.println("Promedio: " + calcularPromedio(notas) + "\n" + "Nota maxima: " + buscarMaximo(notas) + "\n" + "Nota minima: " + buscarMinimo(notas) + "\n" + "Cantidad de Aprobados: " + contarAprobados(notas) );
+        /*System.out.print("Promedio: " + calcularPromedio(notas));
+        System.out.print("estado maximo: " + buscarMaximo(notas));
+        System.out.print("estado minimo: " + buscarMinimo(notas));
+        System.out.print("Cantidad de Aprobados: " + contarAprobados(notas));*/
+    }
 
 }
+//main
+    public static void main(String[] args) {
+        String [] nombresEstudiantes = {"andrea", "luna", "angie", "lucas", "nathaly"};
+        int [] notas = {90, 20, 60, 34, 78};
+        mostrarReporte(nombresEstudiantes, notas);
+    }
+
+}
+
