78d66df67317a7693db3dd1d092526e5448097f8
LuLu <lcuesto@ucompensar.edu.co>
2026-05-15 22:51:02 -0500

    Refactor reto_2 class to rule_2 with updated logic
    Refactor methods to use a new class and update passing criteria to 30. Added estado method to determine pass/fail status.

--- Taller-Java-ulti/src/reto_2.java
+++ Taller-Java-ulti/src/reto_2.java
@@ -1,4 +1,4 @@
-public class reto_2 {
+public class rule_2 {
 
     // metodo calcularPromedio
 public static double calcularPromedio (int [] notas){ 
@@ -29,43 +29,45 @@ public static int buscarMinimo (int [] notas){
     }
     return minimo;
 }
-
-    //HAY QUE USAR FOR-EACH Y CAMBIAR 60 POR 30----------------------CAMBIO
 //metodo contarAprobados
 public static int contarAprobados (int [] notas){
     int sumaAprobados = 0;
     for (int i = 0; i < notas.length; i++) {
-        if (notas[i] >= 60) {
+        if (notas[i] >= 30) {
             sumaAprobados++;
         }
     }
     return sumaAprobados; 
         }
-    // AGREGAR METODO DE ESTADO ARPOBADO Y NO APROBAADO Y ORGANIZAR NOTA MAX, NOTA MIN  Y PROMEDIO-----------------------ARREGLAR
+
+public static String estado (int nota) {
+    if (nota >= 30) {
+        return "Aprobado";
+    } else {
+        return "Reprobado";
+    }
+}
 
     // metodo mostrarReporte
     public static void mostrarReporte (String [] nombresEstudiantes,int [] notas) {
 
-        System.out.println("nombre:" + "\t" + "nota:" + "\t" + "estado:");
-    for (int i = 0; i < nombresEstudiantes.length; i++)
-     {
-         
-       
-        System.out.println( nombresEstudiantes[i] + "\t" +notas[i] + "\t");
-       /* System.out.println("Promedio: " + calcularPromedio(notas) + "\n" + "Nota maxima: " + buscarMaximo(notas) + "\n" + "Nota minima: " + buscarMinimo(notas) + "\n" + "Cantidad de Aprobados: " + contarAprobados(notas) );
-        /*System.out.print("Promedio: " + calcularPromedio(notas));
-        System.out.print("estado maximo: " + buscarMaximo(notas));
-        System.out.print("estado minimo: " + buscarMinimo(notas));
-        System.out.print("Cantidad de Aprobados: " + contarAprobados(notas));*/
+      System.out.println("Nombre:" + "\t" + "\t" + "Nota:" + "\t" + "Estado:");
+      System.out.println("*******************************");
+                for (int i = 0; i < nombresEstudiantes.length; i++)
+{   
+     System.out.println( nombresEstudiantes[i] + "\t" + "\t" + notas[i] + "\t" + estado(notas[i]));
     }
-
-}
+    System.out.println("*******************************");
+    System.out.println("Nota maxima: " + buscarMaximo(notas) + "\s" + "--" + " Nota minima: " + buscarMinimo(notas) + "\n" + "Promedio: " + calcularPromedio(notas) + "\n" + "Aprobados: " + contarAprobados(notas));
+    }
+    
 //main
     public static void main(String[] args) {
+
         String [] nombresEstudiantes = {"andrea", "luna", "angie", "lucas", "nathaly"};
         int [] notas = {90, 20, 60, 34, 78};
+
         mostrarReporte(nombresEstudiantes, notas);
     }
 
 }
-
